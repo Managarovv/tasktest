bc34a7c3347d7ee312a7f148e1ae766d3d1da870
Managarovv <renatkotsan@gmail.com>
2025-11-27 21:50:14 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Wait for PostgreSQL..."
 sleep 5
 
 echo "Applying Prisma migrations..."
-npx prisma migrate dev
+npx prisma migrate dev --name init
 
 echo "Generating Prisma client..."
 npx prisma generate
